308a40493eae991ee0450a7398355929d97e3750
Will <will62185@gmail.com>
2018-09-03 12:59:00 -0400

    added ex6.py

---
+++ ex6.py
@@ -0,0 +1,35 @@
+# Exercise 6 - Strings and Text
+
+# assign 10 to types_of_people
+types_of_people = 10
+# assign the f-string to x (insert types_of_people in the string)
+x = f"There are {types_of_people} types of people."
+
+# assign "binary" to binary
+binary = "binary"
+# assign "don't" to do_not
+do_not = "don't"
+
+y = f"Those who know {binary} and those who {do_not}"
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+
+hailarious = False
+# It seems you need to have an empty curl brace in order to add hailarious
+# since its a bool value into a str value
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+# Changing .format() can break this line 
+print(joke_evaluation.format(hailarious))
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+# Adds var w and e together to print one line
+# Needs to be of same type string to not error
+# Although you can convert a number to a string by e.g. str(2)
+print(w + e)
