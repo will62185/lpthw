4e7a1fc803af8003cd44a81ba94899b4dfd3c630
Will <will62185@gmail.com>
2020-01-12 10:43:34 -0500

    Add ex34.py and .python-version

---
+++ ex34.py
@@ -0,0 +1,30 @@
+# Exercise 34 - Accessing Elements of Lists
+
+animals = ["bear", "python3.6", "peacock", "kangaroo", "whale", "platypus"]
+
+# Cardinal (like "the animal at 1" is 1)
+# Ordinal (1st, 2nd, 3rd) subtact 1
+
+# 1. The animal at 1.
+print(f"The animal at 1 is: {animals[0]}")
+
+# 2. The third (3rd) animal.
+print(f"The third animal is: {animals[2]}")
+
+# 3. The first (1st) animal.
+print(f"The first name is: {animals[0]}")
+
+# 4. The animal at 3.
+print(f"The animal at 3 is: {animals[2]}")
+
+# 5. The fifth (5th) animal.
+print(f"The fifth animal is: {animals[5]}")
+
+# 6. The animal at 2.
+print(f"The animal at 2 is: {animals[1]}")
+
+# 7. The sixth (6th) animal.
+print(f"The sixth animal is: {animals[5]}")
+
+# 8. The animal at 4.
+print(f"The animal at 4 is: {animals[3]}")
